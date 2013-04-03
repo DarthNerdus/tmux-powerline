9330b55fae15e4fc62b3936d9b64d9dd761fd9ab
Jesse Read <darthnerdus@me.com>
2013-04-03 18:28:16 -0400

    Adjusted tmux-mem-cpu-load with required arguments.
    See https://gist.github.com/DarthNerdus/5303901 for my version of the tmux-mem-cpu-load script

--- segments/tmux_mem_cpu_load.sh
+++ segments/tmux_mem_cpu_load.sh
@@ -1,12 +1,12 @@
 # Print out Memory, cpu and load using https://github.com/thewtex/tmux-mem-cpu-load
 
 run_segment() {
-	type tmux-mem-cpu-load >/dev/null 2>&1
+	type tmux-mem-cpu-load 0 0 >/dev/null 2>&1
 	if [ "$?" -ne 0 ]; then
 		return
 	fi
 
-	stats=$(tmux-mem-cpu-load)
+	stats=$(tmux-mem-cpu-load 2 0)
 	if [ -n "$stats" ]; then
 		echo "$stats";
 	fi
